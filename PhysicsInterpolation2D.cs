8653658a3dba848f89eb61b5fcf29a7370d831d1
Tommaso De Tommaso <detommaso23@gmail.com>
2022-09-16 18:48:38 +0200

    Corrected file names

--- PhysicsInterpolation2D.cs
+++ PhysicsInterpolation2D.cs
@@ -6,17 +6,17 @@ public class PhysicsInterpolation2D : EditorPlugin
 {
     public override void _EnterTree()
     {
-        Script Interpolation2DScript = ResourceLoader.Load("res://addons/godot-interpolation2d/Interpolation2D.cs") as Script;
-        Script GenericInterpolation2DScript = ResourceLoader.Load("res://addons/godot-interpolation2d/GenericInterpolation2D.cs") as Script;
+        Script Interpolation2DScript = ResourceLoader.Load("res://addons/godot-interpolation2d/NestedInterpolation2D.cs") as Script;
+        Script GenericInterpolation2DScript = ResourceLoader.Load("res://addons/godot-interpolation2d/Interpolation2D.cs") as Script;
         Texture Interpolation2DTexture = ResourceLoader.Load("res://addons/godot-interpolation2d/Icon.png") as Texture;
         Texture GenericInterpolation2DTexture = ResourceLoader.Load("res://addons/godot-interpolation2d/IconGeneric.png") as Texture;
-        AddCustomType("Interpolation2D", "Node2D", Interpolation2DScript, Interpolation2DTexture);
-        AddCustomType("GenericInterpolation2D", "Node", GenericInterpolation2DScript, GenericInterpolation2DTexture);
+        AddCustomType("NestedInterpolation2D", "Node2D", Interpolation2DScript, Interpolation2DTexture);
+        AddCustomType("Interpolation2D", "Node", GenericInterpolation2DScript, GenericInterpolation2DTexture);
     }
 
     public override void _ExitTree()
     {
+        RemoveCustomType("NestedInterpolation2D");
         RemoveCustomType("Interpolation2D");
-        RemoveCustomType("GenericInterpolation2D");
     }
 }
