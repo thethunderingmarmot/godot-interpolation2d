d410dd9657dbcfa9ce6f3b2d3c831d46609f164d
Tommaso De Tommaso <detommaso23@gmail.com>
2022-09-16 18:47:58 +0200

    Corrected directory name

--- PhysicsInterpolation2D.cs
+++ PhysicsInterpolation2D.cs
@@ -6,10 +6,10 @@ public class PhysicsInterpolation2D : EditorPlugin
 {
     public override void _EnterTree()
     {
-        Script Interpolation2DScript = ResourceLoader.Load("res://addons/PhysicsInterpolation2D/Interpolation2D.cs") as Script;
-        Script GenericInterpolation2DScript = ResourceLoader.Load("res://addons/PhysicsInterpolation2D/GenericInterpolation2D.cs") as Script;
-        Texture Interpolation2DTexture = ResourceLoader.Load("res://addons/PhysicsInterpolation2D/Icon.png") as Texture;
-        Texture GenericInterpolation2DTexture = ResourceLoader.Load("res://addons/PhysicsInterpolation2D/IconGeneric.png") as Texture;
+        Script Interpolation2DScript = ResourceLoader.Load("res://addons/godot-interpolation2d/Interpolation2D.cs") as Script;
+        Script GenericInterpolation2DScript = ResourceLoader.Load("res://addons/godot-interpolation2d/GenericInterpolation2D.cs") as Script;
+        Texture Interpolation2DTexture = ResourceLoader.Load("res://addons/godot-interpolation2d/Icon.png") as Texture;
+        Texture GenericInterpolation2DTexture = ResourceLoader.Load("res://addons/godot-interpolation2d/IconGeneric.png") as Texture;
         AddCustomType("Interpolation2D", "Node2D", Interpolation2DScript, Interpolation2DTexture);
         AddCustomType("GenericInterpolation2D", "Node", GenericInterpolation2DScript, GenericInterpolation2DTexture);
     }
